9071496ae59163ceca5c8a5c7fa77966b40e34fb
Doc-ER <waynejszhang@outlook.com>
2017-05-25 18:50:18 +0800

    无

--- PyForHR/calculatePromote.py
+++ PyForHR/calculatePromote.py
@@ -1,4 +1,25 @@
 #/usr/local/bin
 import pandas as pd
-import numpy as np
-df = pd.read_excel("testData.xls", sheetname = "Sheet1")
+#import numpy as np
+import re
+
+df = pd.read_excel("testData.xls", sheetname = "Sheet1", index_col=0)
+
+def getThePromote(data = data, groups = groups, grade = grade, weight = weight):
+	for group in groups:
+	groupCol = list()
+		for col in data.columns:
+			if re.match(group, col):
+				groupCol.append(col)
+	groupData = data.loc[:, groupCol]
+	
+
+
+
+
+
+
+
+	
+	
+
